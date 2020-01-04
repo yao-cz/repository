dabd47f94b389e88e01339bcc59b321bbf37a4fc
yao-cz <59494221+yao-cz@users.noreply.github.com>
2020-01-04 11:40:08 +0800

    Create MyRestController.java

---
+++ MyRestController.java
@@ -0,0 +1,13 @@
+package ydp.day01.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyRestController {
+    String value() default "";
+}
