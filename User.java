1c9d7ffaa71182b504ae7bbfe323b0529d92bb3a
yao-cz <59494221+yao-cz@users.noreply.github.com>
2020-01-04 11:41:21 +0800

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+package ydp.day01.Test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@ToString
+public class User {
+    private int id;
+    private String name;
+}
